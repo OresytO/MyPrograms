a0cf8506b0b08ce62aeb4df770f23f9cfdb2211e
Orest <orecto@gmail.com>
2014-06-28 12:26:27 +0300

    Resolving some warnings issues

--- Workspace/rebok2j/src/main/java/org/rebok2j/Runner.java
+++ Workspace/rebok2j/src/main/java/org/rebok2j/Runner.java
@@ -2,9 +2,6 @@ package org.rebok2j;
 
 public class Runner {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) throws Exception {
        /* Server server = new Server(8080);
 
