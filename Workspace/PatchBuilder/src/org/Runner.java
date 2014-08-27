0690ade6ae1803f694eb159b5b06566a4a5293f8
unknown <olozyn@VAN.softservecom.com>
2014-08-27 06:52:32 -0500

    27/08/2014(1)

--- Workspace/PatchBuilder/src/org/Runner.java
+++ Workspace/PatchBuilder/src/org/Runner.java
@@ -1,30 +1,66 @@
 package org;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.patchBuilder.msOffice.Excel;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.patchBuilder.jdbc.Data;
+/**
+ * Created by olozyn on 6/27/2014.
+ */
 
 public class Runner {
 
-    public static void main(String[] args) throws IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("primary");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        System.out.println("1");
-        ArrayList<Data> list = new ArrayList<>();
-        list = (ArrayList<Data>) em.createNamedQuery("First_Query").getResultList();
+  public static final String INPUT_FILE_PATH1 = "C:\\Users\\olozyn\\Desktop\\Copy of R-IT-Chcklst.xls";
+  public static final String INPUT_FILE_PATH2 = "D:\\working\\3.6.0\\Trunk\\blue-web\\schawk-parts-web\\src\\resources\\settingsdescriptions.properties";
+  public static final String OUTPUT_FILE_PATH = "D:\\output\\settingsdescriptions.properties";
 
-        System.out.println("1");
-        for (Data temp : list) {
-            System.out.println(temp);
-        }
+  public static void main(String[] args) {
+    System.out.println("BEGIN!!!");
+    // ------getting infomation from file-------- //
+    Row row = Excel.open(INPUT_FILE_PATH1, 9, 1);
+    if (row == null)
+      throw new RuntimeException("row == null :(");
+    // List<String> headers = Excel.getAll(row);
+    Sheet sheet = Excel.getSheet();
 
-        System.out.println("1");
-        em.getTransaction().commit();
+    Map<String, String> mapFromExcel = createMap(sheet, 1, 173);
+
+    File output = new File(OUTPUT_FILE_PATH);
+    try (FileWriter fw = new FileWriter(output)) {
+      for (String key : mapFromExcel.keySet()) {
+        String str = key + "=" + mapFromExcel.get(key) + "\n";
+        fw.write(str);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    System.out.println("END!!!");
+
+  }
+
+  private static Map<String, String> createMap(Sheet sheet, int fromRow, int toRow) {
+    if (fromRow > toRow) {
+      int temp = fromRow;
+      fromRow = toRow;
+      toRow = temp;
+    }
+    HashMap<String, String> map = new LinkedHashMap<>();
+    for (Row row : Excel.getRows(sheet)) {
+      if (row.getRowNum() >= fromRow && row.getRowNum() <= toRow) {
+        String key = Excel.get(row, 0);
+        String value = Excel.get(row, 4).replaceAll("\n", " ");
+        if (key.isEmpty()) continue;
+        map.put(key, value);
+      } else if (row.getRowNum() > toRow)
+        break;
+    }
+    return map;
+  }
 }
---
+++ Workspace/TestTest/src/RunnerMain.java
@@ -0,0 +1,53 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+public class RunnerMain {
+
+    public static final String INPUT_FILE_PATH1 = "C:\\Users\\olozyn\\Desktop\\Copy of R-IT-Chcklst.xls";
+    public static final String INPUT_FILE_PATH2 = "D:\\working\\3.6.0\\Trunk\\blue-web\\schawk-parts-web\\src\\resources\\settingsdescriptions.properties";
+    public static final String OUTPUT_FILE_PATH = "D:\\output\\settingsdescriptions.properties";
+
+    public static void main(String[] args) {
+        System.out.println("BEGIN!!!");
+        // ------getting infomation from file-------- //
+        // Row row = Excel.open(INPUT_FILE_PATH1, 10, 1);
+        // if (row == null)
+        // throw new RuntimeException("row == null :(");
+        // List<String> headers = Excel.getAll(row);
+        Sheet sheet = Excel.getSheet();
+        Map<String, String> mapFromExcel = createMap(sheet, 1, 174);
+
+//        File output = new File(OUTPUT_FILE_PATH);
+//        try (FileWriter fw = new FileWriter(output)) {
+//            for (String key : mapFromExcel.keySet())
+//                fw.write(key + "=" + mapFromExcel.get(key));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("END!!!");
+
+    }
+
+    private static Map<String, String> createMap(Sheet sheet, int fromRow, int toRow) {
+        if (fromRow > toRow) {
+            int temp = fromRow;
+            fromRow = toRow;
+            toRow = temp;
+        }
+        HashMap<String, String> map = new HashMap<>();
+        for (Row row : Excel.getRows(sheet)) {
+            if (row.getRowNum() >= fromRow && row.getRowNum() <= toRow) {
+                map.put(Excel.get(row, 1), Excel.get(row, 5));
+            } else if (row.getRowNum() > toRow)
+                break;
+        }
+        return map;
+    }
+}
