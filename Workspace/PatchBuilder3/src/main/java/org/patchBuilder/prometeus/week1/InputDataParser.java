8e9deb50f1502cb3a44d20fdc285d1c8e66f4c24
Orest <orecto@gmail.com>
2015-02-25 01:49:26 +0200

    make order in my repo

---
+++ Workspace/PatchBuilder3/src/main/java/org/patchBuilder/prometeus/week1/InputDataParser.java
@@ -0,0 +1,76 @@
+package org.patchBuilder.prometeus.week1;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author olozynskyy
+ * @since 3.7.0.
+ */
+public class InputDataParser {
+    public static List<ParseResult> parse(InputStream inputStream) throws IOException {
+        List<ParseResult> result = new ArrayList<>();
+        ParseResult parseResult = new ParseResultImpl();
+        Scanner scanner = new Scanner(inputStream);
+        String line;
+
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            if (line.isEmpty())
+            {
+                result.add(parseResult);
+                parseResult = new ParseResultImpl();
+            }
+            parse(line, parseResult);
+        }
+
+        scanner.close();
+        inputStream.close();
+        return result;
+    }
+
+    private static ParseResult parse(String line, ParseResult parseResult)
+    {
+        boolean isValueCounter = false;
+        Map<Integer, List<Integer>> counterMap = null;
+        Integer mapKey = null;
+        List<Integer> valuesList;
+
+        if (isValueCounter && !line.isEmpty()) {
+            mapKey = Integer.valueOf(line.substring(0, line.indexOf(" ")));
+            valuesList = new ArrayList<>();
+
+            int valStart= line.indexOf(" [") + 2;
+            int valEnd= line.indexOf(", ");
+            valuesList.add(Integer.valueOf(line.substring(valStart, valEnd)));
+            valStart = valEnd + 2;
+            valEnd = line.indexOf(", ", valStart);
+            valuesList.add(Integer.valueOf(line.substring(valStart, valEnd)));
+            valStart = valEnd + 2;
+            valEnd = line.indexOf("]", valStart);
+            valuesList.add(Integer.valueOf(line.substring(valStart, valEnd)));
+
+            counterMap.put(mapKey, valuesList);
+            parseResult.setCounterMap(counterMap);
+        } else if (line.isEmpty()) {
+            isValueCounter = false;
+        } else if (line.contains("X: ")) {
+            parseResult.setValueX(line.substring(3, line.length() - 1));
+        } else if (line.contains("Y: ")) {
+            parseResult.setValueY(line.substring(3, line.length() - 1));
+        } else if (line.contains("Z: ")) {
+            parseResult.setValueZ(line.substring(3, line.length() - 1));
+        } else if (line.contains("Value counter")) {
+            isValueCounter = true;
+            counterMap = new HashMap<>();
+        }
+
+        return parseResult;
+    }
+}
---
+++ Workspace/PatchBuilder3/src/main/java/org/patchBuilder/prometeus/week1/Problem1.java
@@ -0,0 +1,20 @@
+package org.patchBuilder.prometeus.week1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by OrestO
+ * on 2/24/2015.
+ */
+public class Problem1
+{
+    public static void main(String ... args) throws IOException {
+        String filePath = "D:\\TOP SECRET\\Top secret\\MyPrograms\\MyPrograms\\Workspace\\PatchBuilder3\\src\\main\\java\\InputForAlgo\\Week1Problem1.txt";
+//        String filePath = "InputForAlgo/Week1Problem1.txt";
+//        ParseResult parseResult = InputDataParser.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath));
+        List<ParseResult> parseResult = InputDataParser.parse(new FileInputStream(filePath));
+        System.out.println(parseResult.get(0).getCounterMap());
+    }
+}
