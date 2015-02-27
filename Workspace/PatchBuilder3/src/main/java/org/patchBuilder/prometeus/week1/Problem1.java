402e9701afe8b382de81f1a889557d4a73d54c0d
oresyto <orecto@gmail.com>
2015-02-27 16:55:39 +0200

    commit before pull

--- Workspace/PatchBuilder3/src/main/java/org/patchBuilder/prometeus/week1/Problem1.java
+++ Workspace/PatchBuilder3/src/main/java/org/patchBuilder/prometeus/week1/Problem1.java
@@ -11,10 +11,10 @@ import java.util.List;
 public class Problem1
 {
     public static void main(String ... args) throws IOException {
-        String filePath = "D:\\TOP SECRET\\Top secret\\MyPrograms\\MyPrograms\\Workspace\\PatchBuilder3\\src\\main\\java\\InputForAlgo\\Week1Problem1.txt";
-//        String filePath = "InputForAlgo/Week1Problem1.txt";
-//        ParseResult parseResult = InputDataParser.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath));
-        List<ParseResult> parseResult = InputDataParser.parse(new FileInputStream(filePath));
+//        String filePath = "D:\\TOP SECRET\\Top secret\\MyPrograms\\MyPrograms\\Workspace\\PatchBuilder3\\src\\main\\java\\InputForAlgo\\Week1Problem1.txt";
+        String filePath = "InputForAlgo/Week1Problem1.txt";
+      List<ParseResult> parseResult = InputDataParser.parse(Thread.currentThread().getContextClassLoader().getResource(filePath).openStream());
+//        List<ParseResult> parseResult = InputDataParser.parse(new FileInputStream(filePath));
         System.out.println(parseResult.get(0).getCounterMap());
     }
 }
